a2d1182704e23a14de6d41f0abc7e0e2dbd10455
Revanthmk <38763740+Revanthmk@users.noreply.github.com>
2021-05-03 20:24:01 +0530

    Update mountain_car.cpp

--- mountain_car.cpp
+++ mountain_car.cpp
@@ -1,4 +1,4 @@
-/* This program is to evaluate derivative using 2 point method for different step sizes 'h' 
+/* This program is to evaluate derivative using 3 point method for different step sizes 'h' 
 with single precision and double precision and compare it with the actual value to 
 evaluate the absolute error involved*/
 #include <iostream>
@@ -17,7 +17,7 @@ double func2(double x)
 }
 int main()
 {
-ofstream file("2pt_error.dat");
+ofstream file("3pt_error.dat");
 double N = 1;
 double a = 10;
 float exact1;
@@ -26,7 +26,7 @@ file<<"Single precision"<<endl;
 file<<"h"<<"        "<<"Absolute error"<<endl;
 for (N=1;N<=pow(10,8);N=N*10){
     float h1 = a/N;
-    float diff1 = (func1((pow(2,0.5)+h1))-func1(pow(2,0.5)))/h1;
+    float diff1 = (func1((pow(2,0.5)+h1))-func1((pow(2,0.5))-h1))/(2*h1);
     cout<<diff1<<endl;
     file<<h1<<"      "<<abs((diff1-exact1))<<endl;
 }
@@ -37,7 +37,7 @@ file<<"Double precision"<<endl;
 file<<"h"<<"        "<<"Absolute error"<<endl;
 for (N=1;N<=pow(10,16);N=N*10){
     double h2 = a/N;
-    double diff2 = (func2((pow(2,0.5)+h2))-func2(pow(2,0.5)))/h2;
+    double diff2 = (func2((pow(2,0.5)+h2))-func2((pow(2,0.5))-h2))/(2*h2);
     cout<<diff2<<endl;
     file<<h2<<"      "<<abs((diff2-exact2))<<endl;
 }
